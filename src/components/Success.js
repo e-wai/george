b1ae706d87ca18402d481b35b3a8fd4c2f349d0a
Luke Son <flare2930@gmail.com>
2020-11-22 06:44:28 -0700

    final version?

--- src/components/Success.js
+++ src/components/Success.js
@@ -1,15 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Main from './Main.js';
-import Modal from 'react-bootstrap/Modal';
+import Modal from 'react-bootstrap/Modal'
+import fire from '../firebase.js';;
 
 const Success = () => {
 
     const [show, setModalVisible] = useState(true);
 
+    const [userUID, setUserUID] = useState("");
+    const [userData, setUserData] = useState({});
+
+    const usersRef = fire.firestore().collection('users')
+    
+
+
+    useEffect(() => {
+        fire.auth().onAuthStateChanged(user => {
+            if (user) {
+              console.log("user" + user.uid)
+              setUserUID(user.uid);
+            } else {
+                window.location.href = "http://localhost:3000/register"
+            }
+        });
+    }, [])
+
+
+    useEffect(() => {
+        // make sure userUID is obtained before data is retrieved
+        if(userUID) {
+            usersRef.doc(userUID).get()
+            .then(userReturned => {
+                if(!userReturned.exists) {
+                    return;
+                } else {
+                    setUserData(userReturned.data());
+                }
+            })
+        }
+
+    }, [userUID])
+
     const handleClose = () => {
-        window.location.href = "http://localhost:3000/main";
+        usersRef.doc(userData.id).get().then(documentSnapshot => {
+            usersRef.doc(userData.id).update({
+                items: []
+            }).then(() => {
+                window.location.href = "http://localhost:3000/main";
+            }).catch(error => {
+                console.log(error);
+            })
+        })
         setModalVisible(false);
     }
+
+
 
     return (
         <div className="successDiv">
